30d8eaa0b6e702ea6116fac67b7e49b65d02af97
Steve Chan <chanshunli@gmail.com>
2017-09-15 15:30:27 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-easy-ocr
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_easy_ocr/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-easy-ocr.core-test
+  (:require [clojure.test :refer :all]
+            [clj-easy-ocr.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
